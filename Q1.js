dd13d3d4b87dfd19aa93f9758426400d94d1e597
Luis Ballote <ealjkl@gmail.com>
2022-06-13 16:01:36 -0500

    Fixed Q1 and Q2

--- Q1.js
+++ Q1.js
@@ -1,35 +1,47 @@
-let words = ["hello", "bye", "thanks", "something", "else", "carrot", "avocado"];
+let words = [
+  "hello",
+  "bye",
+  "thanks",
+  "something",
+  "else",
+  "carrot",
+  "avocado",
+];
 
 function isConsonant(ch) {
-    let vowels = ["a", "e", "i", "o", "u"]; 
-    return !vowels.includes(ch); 
+  let vowels = ["a", "e", "i", "o", "u"];
+  return !vowels.includes(ch);
+}
+
+function numConst(tot, ch) {
+  tot += isConsonant(ch);
+  return tot;
 }
 
 //the sorting is in-place, because the sort method is in-place, but it can easily be converted into a non-in-place funciton
-function sortWords(words, option="lexicographic", ascending=true) {
-    let compareFn;
-    if (option === "lexicographic") {
-        return words.sort();
-    } else if (option === "length") {
-        compareFn = (w1, w2) => (w1.length - w2.length); 
-    } else if (option === "consonants") {
-        compareFn = (w1, w2) => {
-            function numConst(tot, ch) {
-                tot += isConsonant(ch);
-                return tot;
-            } 
-            let cons1 = Array.prototype.reduce.call(w1, numConst, 0);
-            let cons2 = Array.prototype.reduce.call(w2, numConst, 0);
-            
-            return cons1 - cons2;
-        }
-    }
-    return words.sort((w1, w2) => {
-        return (ascending ? 1 : -1) * compareFn(w1, w2);
-    }); 
+function sortWords(words, option = "lexicographic", ascending = true) {
+  let compareFn;
+  if (option === "lexicographic") {
+    return words.sort();
+  } else if (option === "length") {
+    compareFn = (w1, w2) => w1.length - w2.length;
+  } else if (option === "consonants") {
+    compareFn = (w1, w2) => {
+      let cons1 = Array.prototype.reduce.call(w1, numConst, 0);
+      let cons2 = Array.prototype.reduce.call(w2, numConst, 0);
+
+      return cons1 - cons2;
+    };
+  }
+  if (!ascending) finalCompareFn = (w1, w2) => -1 * compareFn(w1, w2);
+  else finalCompareFn = compareFn;
+
+  return words.sort((w1, w2) => {
+    return finalCompareFn(w1, w2);
+  });
 }
 
 console.log(sortWords(words, "lexicographic"));
-console.log(sortWords(words, "length")); 
-console.log(sortWords(words, "consonants")); 
-console.log(sortWords(words, "consonants", false)); 
+console.log(sortWords(words, "length"));
+console.log(sortWords(words, "consonants"));
+console.log(sortWords(words, "consonants", false));
--- Q2.js
+++ Q2.js
@@ -1,10 +1,8 @@
 function limitFunc(fn, num) {
-    let count = 0;
-    return function() {
-        if (count++ < num) {
-            fn(...arguments); 
-        }
-    };
+  let count = 0;
+  return function (...args) {
+    if (count++ < num) return fn(...args);
+  };
 }
 
 let limited = limitFunc(() => console.log("hi!"), 2);
@@ -12,3 +10,8 @@ let limited = limitFunc(() => console.log("hi!"), 2);
 limited();
 limited();
 limited();
+
+let limitedSum = limitFunc((a, b) => a + b, 2);
+console.log(limitedSum(4, 5));
+console.log(limitedSum(2, 1));
+console.log(limitedSum(3, 10));
